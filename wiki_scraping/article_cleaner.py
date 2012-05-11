98daff9eb0ba4f31dbd4410c70aaab16584b5584
Jay Baxter <jbaxter@mit.edu>
2012-05-11 16:55:33 -0400

    added way to get infobox data

--- wiki_scraping/article_cleaner.py
+++ wiki_scraping/article_cleaner.py
@@ -45,10 +45,10 @@ def make_clean_dataset_directory(src_dir, target_dir):
             f = open(target_dir + '/' + subdir + '/' + filename, 'w')
             f.write(new_text)
             
-def make_topic_dataset_pickle(src_dir, target_file):
+def make_topic_dataset(src_dir):
     """Take in a dataset directory src_dir, which should have subdirectories that represent
     categories, and files in those subdirectories that are plaintext wikipedia articles.
-    Clean those wikipedia articles and put them in a new dataset directory called target_dir."""
+    Clean those wikipedia articles return them."""
     listing = os.listdir(src_dir)
     topic_struct = dict()
     for subdir in listing:
@@ -64,8 +64,56 @@ def make_topic_dataset_pickle(src_dir, target_file):
             topic_headings = [re.sub(r'&lt;.*?&gt;', '', h) for h in topic_headings]
             topic_headings = [re.sub(r'&[^;\s]*?;', '', h) for h in topic_headings]
             topic_struct[subdir][filename] = topic_headings
-    pickle.dump(topic_struct, open(target_file, 'w'))
     return topic_struct
+    
+def make_infobox_dataset(src_dir):
+    """Take in a raw dataset directory, and extract infobox data."""
+    listing = os.listdir(src_dir)
+    infobox_struct = dict()
+    
+    for subdir in listing:
+        infobox_struct[subdir] = dict()
+        files = os.listdir(src_dir + '/' + subdir)
+        for filename in files:
+            orig_text = open(src_dir + '/' + subdir + '/' + filename).read().decode('utf-8')
+            
+            infobox_features = []
+            infobox = False
+            ignore = 0
+            for line in orig_text.splitlines():
+                if not infobox:
+                    match = re.search(r'{{Infobox',line)
+                    if match:
+                        infobox = True
+                        infobox_features.append(match.string[10:])
+                if infobox:
+                    print line
+                    # extract the features from the line
+                    if len(line) > 1 and line[0] == '|':
+                        match = re.search(r'\s?.*?\s*?=', line[1:])
+                        if match:
+                            feature_name = re.sub(r'=.*', "", match.string)
+                            infobox_features.append(feature_name)
+                    
+                    # ignore enclosed multiline double-braces
+                    if re.search(r'{{',line) and not re.search(r'{{Infobox',line):
+                        ignore += 1
+                    if re.search(r'}}',line):
+                        if not re.search(r'{{',line):
+                            if not ignore:
+                                break 
+                        # ignore enclosed single-line double-braces
+                        ignore -= 1
+            if len(infobox_features) > 0:
+                infobox_struct[subdir][filename] = infobox_features
+    return make_infobox_dataset
+    
+def make_combined_dataset_pickle(raw_dir, clean_dir, target_file):
+    """Take in a raw dir and a cleaned dir, and generate a combined pickle file
+    that contains both infobox data and topic data."""
+    infobox_struct = make_infobox_dataset(raw_dir)
+    topic_struct = make_topic_dataset(clean_dir)
+    
                 
 if __name__=='__main__':
     if (len(sys.argv) < 3) or (not os.path.exists(sys.argv[1])):
