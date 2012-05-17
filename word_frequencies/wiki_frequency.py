4f442686e6841655dd57d16678481dd175463ddc
Shahar Ronen <shahar.ronen@gmail.com>
2012-05-17 03:19:16 -0400

    fixed notes, imports, comments; moved evaluation to separate module; classify_article_tfidf() now returns name of best match in addition to scores for all matches

--- word_frequencies/wiki_frequency.py
+++ word_frequencies/wiki_frequency.py
@@ -3,39 +3,36 @@ cmd_folder = os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe(
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
-import nltk
-from nltk.corpus import stopwords
 import time
-from collections import defaultdict
-from collections import Counter
-from nltk.corpus import PlaintextCorpusReader
-from nltk.corpus import TaggedCorpusReader
-from nltk.corpus import CorpusReader
 import operator
 import codecs
+from collections import defaultdict, Counter
+
+import nltk
+from nltk.corpus import CorpusReader
+
 import tf_idf
 import cosine_similarity as cosine_sim
+import str_corpus_cleaner, pos_corpus_cleaner
 
 
 # TODO: split set to training and test, and evaluate. maybe 70:30 training:test?
 
-def calculate_corpus_tf_idf(pos_clean_root):
+def calculate_corpus_tf_idf(corpus_reader):
 	'''
 	Calculate TF*IDF weight for each document in given corpus.
+	-Input: CorpusReader (either Tagged or PlainText )
 	-Return:
 	 (1) A dictionary whose keys=document name and values=dictionary
 	 with terms for keys and TF*IDF weights for values
 	 (2) A dictionary whose keys=terms and values=their IDF
 	'''
 	st_time = time.time()
-	
-	# Read the tagged corpus
-	whole_corpus = TaggedCorpusReader(pos_clean_root, '.*\.pos.clean')
-	
+		
 	# Term Frequency for each category
 	tfs_per_document = defaultdict(Counter)
-	for document in whole_corpus.fileids():
-		terms_in_document = whole_corpus.words(document)
+	for document in corpus_reader.fileids():
+		terms_in_document = corpus_reader.words(document)
 		tfs_per_document[document] = tf_idf.tf(terms_in_document)
 		
 	# Inverse Document Frequency
@@ -46,31 +43,19 @@ def calculate_corpus_tf_idf(pos_clean_root):
 	for document, tfs in tfs_per_document.iteritems():
 	    tfidfs_per_document[document] = tf_idf.tf_idf(tfs, idfs, len(tfs_per_document))
 		
-	for document, values in tfidfs_per_document.iteritems():
-		print document
-		print top_n_terms(values, 20)
-		print
-		
-	print "time to compute:", time.time()-st_time
+	print "time to compute TF-IDF weights for corpus: %.3f sec" % (time.time()-st_time)
 	return tfidfs_per_document, idfs
 
 
-def classify_article_tfidf(article, corpus_tfidfs_per_category, corpus_idf):
+def classify_article_tfidf(article_words, corpus_tfidfs_per_category, corpus_idf):
 	'''
 	Find the category that best matches the given article among the given
 	categories.
-	-Input: (1) article path (2) TF*IDF weights for each document in corpus,
-	 (3) IDF for the entire corpus
+	-Input: (1) list of article terms (2) TF*IDF weights for each document in
+	 corpus (3) IDF for the entire corpus
 	-Return: a dictionary with match score for the article with each category
 	'''
 	st_time = time.time()
-	
-	ar_text = codecs.open(article, 'rU').read()
-	
-	article_tagged = nltk.pos_tag(nltk.wordpunct_tokenize(ar_text))
-	article_taggged_clean = clean_pos(article_tagged)
-	article_words = [word for (word,pos) in article_taggged_clean]
-	#article_corpus = nltk.Text(article_words)
 	
 	# Find article TF and TFIDF
 	article_tfs = tf_idf.tf(article_words)
@@ -83,12 +68,18 @@ def classify_article_tfidf(article, corpus_tfidfs_per_category, corpus_idf):
 		sim[cat_name] = \
 			cosine_sim.cosine_similarity_dict(article_tfidfs, cat_tfidf_scores)
 		
-	# sort by value (match score), ascending
-	match = sorted(sim.iteritems(), key=operator.itemgetter(1), reverse=True)
-	print "match:", match[0]
-	print "time to match:", time.time()-st_time
+	# sort by value (match score), descending
+	match = sorted(sim.iteritems(), key=operator.itemgetter(1), reverse=True)[0]
 	
-	return sim
+	return match, sim
+
+
+def print_top_terms(tfidfs, num=20):
+	for document, terms in tfidfs.iteritems():
+		print document
+		sorted_by_count_top = sorted(terms.iteritems(), key=operator.itemgetter(1), reverse=True)[:num]
+		for pair in sorted_by_count_top:
+			print '  ', pair
 
 
 if __name__ == "__main__":	
@@ -98,16 +89,26 @@ if __name__ == "__main__":
 		print "Usage: python wiki_frequency.py <file or folder to load files from into corpus>"
 		exit()
 	
-	tfidfs_per_doc, idfs = wiki_frequency.calculate_corpus_tf_idf('../datasets/cleaned_featured_bios/_pos_clean_/')
+	# Initialize a plain-text corpus 
+	training_corpus = nltk.corpus.PlaintextCorpusReader( \
+		'../datasets/cleaned_featured_bios/_filtered_3_plus/', '.*\.txt')
+	# Alternatively, use a POS-tagged corpus
+	# corpus = nltk.corpus.TaggedCorpusReader( \
+	#	'../datasets/cleaned_featured_bios/_filtered_3_plus/')
 	
-	sim = classify_article_tfidf('../datasets/cleaned_featured_bios/Media biographies/Cillian_Murphy.txt', tfidfs_per_doc, idfs)
+	# Calculate TF*IDF for each category and IDF for the entrie corpus
+	tfidfs_per_doc, idfs = calculate_corpus_tf_idf(training_corpus)
+	#print_top_terms(tfidfs_per_doc)
+	
+	# Initialize new article
+	ar_text = codecs.open(root_path+'/'+'Media biographies/Cillian_Murphy.txt', 'rU').read()
+	article_words = str_corpus_cleaner.get_clean_terms(ar_text)	
+	print "STR"
+	math, sim = classify_article_tfidf(article_words, tfidfs_per_doc, idfs)
 		
-	# Finally - for each article / paragraph, run a cosine similarity of words extract with words in each category to classify?
-	# get terms from article
-	'''
-	sim = calc_similarity(root_path+'Media biographies/Cillian_Murphy.txt', all_probs, prob_ratios)
-	sim = calc_similarity(root_path+'Music biographies/AC_DC.txt', all_probs, prob_ratios)
-	sim = calc_similarity(root_path+'Literature and theatre biographies/Anton_Chekhov.txt', all_probs, prob_ratios)
-	sim = calc_similarity(root_path+'Sport and recreation biographies/Tim_Duncan.txt', all_probs, prob_ratios)
-	sim = calc_similarity('../datasets/Tiger_Woods.txt', all_probs, prob_ratios)
-	'''
+	# Alternatively, use the POS cleaner
+	article_tagged = nltk.pos_tag(nltk.wordpunct_tokenize(ar_text))
+	article_taggged_clean = pos_corpus_cleaner.clean_pos(article_tagged)
+	article_words = [word for (word,pos) in article_taggged_clean]
+	print "POS"
+	match, sim = classify_article_tfidf(article_words, tfidfs_per_doc, idfs)
