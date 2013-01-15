970c08726d18b95350968a85ee18214f1f8e6df1
Shahar Ronen <shahar.ronen@gmail.com>
2013-01-15 18:17:43 -0500

    language codes are written in alphabetical order (e.g., 'en' always preceds 'es')

--- retrieve/filter_specified_langs.py
+++ retrieve/filter_specified_langs.py
@@ -3,11 +3,17 @@ Get a tab-and-comma-separated list of the following format:
  lang_code1,article_name1\tlang_code2,article_name2\t...
 
 Return a file of the same format with only the selected languages.
-Script assumes all lines contains all the languages that are set to be kept.
+NOTE: the script assumes all lines contains all the languages that should be kept.
+E.g., if English and Spanish are to be kept, each line contains at least English 
+and Spanish.
+
+Languages are sorted arphabetically in the returned file.
 '''
 
-INFILE = "english_spanish_people.tsv"
-OUTFILE = "english_spanish_people_only.tsv"
+from collections import defaultdict
+
+INFILE = "english_spanish_people_only.tsv" # test file
+OUTFILE = "parallel_eng_spa.tsv"
 LANGS_TO_KEEP = ["en", "es"]
 SEP = "||"
 
@@ -16,17 +22,24 @@ fout = open(OUTFILE, "w")
 
 for line in fin:
 	wiki_editions = line.rstrip().split('\t')
+	kept_editions = defaultdict()
 
 	for edition in wiki_editions:
-		#print edition
 		try:
 			lang_code, article_name = edition.split(SEP)
+
 			if lang_code in LANGS_TO_KEEP:
-				fout.write("%s%s%s\t" % (lang_code,SEP,article_name))
+				kept_editions[lang_code] = article_name
+				#print "Added:", lang_code, article_name
+
 		except ValueError:
 			# probably because a comma was used in the person's name
 			print "Error (extra comma?): %s" % edition
 			continue
+
+	 
+	for lang_code in sorted(kept_editions):
+		fout.write("%s%s%s\t" % (lang_code,SEP,kept_editions[lang_code]))
 
 	fout.write("\n")
 
