2b91a2bf56c2e18bdada2eaf1ef21de0fd84aed7
Shahar Ronen <shahar.ronen@gmail.com>
2013-05-30 02:44:35 -0400

    drawing topic div by category; color donuts; removed old code

--- paper_cscw/category_stats.R
+++ paper_cscw/category_stats.R
@@ -1,6 +1,3 @@
-# English entropy: 2.714043
-# Spanish entropy: 2.662469
-
 # Entropy equation for latex:
 
 #\usepackage{amsmath}
@@ -28,7 +25,6 @@ prep.category.stats.table <- function(topics.df, language.name) {
 }
 
 plot.donuts <- function(cat.stats) {
-  cat.stats <- all.stats
   # plot a category donut chart for each language
   p.en <- plot.category.shares.donut(subset(cat.stats, Language=="English"), "English")
   p.es <- plot.category.shares.donut(subset(cat.stats, Language=="Spanish"), "Spanish")
@@ -47,7 +43,7 @@ plot.category.shares.donut <- function(dat, lang) {
   dat$ymin = c(0, head(dat$ymax, n=-1)) #  
   
   donut.text <- dat$percent.articles/2 + c(0, cumsum(dat$percent.articles)[-length(dat$percent.articles)])
-  donut.text.size <- 3 # make it small for easier post-processing
+  donut.text.size <- dat$percent.articles * 0.5 # 3 # make it small for easier post-processing
   
   # environment() helps pass variables for geom_text labels,
   # see http://stackoverflow.com/questions/15429447/why-does-my-user-supplied-label-in-geom-text-in-ggplot2-generate-an-error
@@ -57,7 +53,7 @@ plot.category.shares.donut <- function(dat, lang) {
     geom_rect(colour="grey30") +
     coord_polar(theta="y") +
     xlim(c(0, 4)) +
-    scale_fill_grey(start = 0, end = .9) +
+    #scale_fill_grey(start = 0, end = .9) +
     geom_text(aes(x=3.5,y=donut.text, # x is between xmax and xmin
                   label=sprintf("%s\n%s%%", category, percent.articles)), 
               size=donut.text.size) +
@@ -72,100 +68,36 @@ plot.category.shares.donut <- function(dat, lang) {
 
 
 ### MAIN ####
-# source('../datasets/prep_data.R', chdir=T)
+#source('../datasets/prep_data.R', chdir=T)
 
+## Category shares
 en.cat.stats <- prep.category.stats.table(en.article.stats, "English")
 es.cat.stats <- prep.category.stats.table(es.article.stats, "Spanish")
 it.cat.stats <- prep.category.stats.table(it.article.stats, "Italian")
 pt.cat.stats <- prep.category.stats.table(pt.article.stats, "Portuguese")
 
-all.stats <- rbind(en.cat.stats, es.cat.stats,
+all.cat.stats <- rbind(en.cat.stats, es.cat.stats,
                    it.cat.stats, pt.cat.stats)
 
-postscript("category_share_comparison.eps")
-plot.donuts(all.stats)
+postscript("category_share_comparison.eps", height=900, width=900)
+plot.donuts(all.cat.stats)
 dev.off()
+exit()
 
-## load tables
-# en.topic.shares <- load.my.table(sprintf(TOPIC.SHARES.TBL.PATH, "en"))
-# es.topic.shares <- load.my.table(sprintf(TOPIC.SHARES.TBL.PATH, "es"))
-# it.topic.shares <- load.my.table(sprintf(TOPIC.SHARES.TBL.PATH, "it"))
-# pt.topic.shares <- load.my.table(sprintf(TOPIC.SHARES.TBL.PATH, "pt"))
-# get per-category stats
-# all.stats <- rbind(category.stats(en.topic.shares, "English"),
-#                    category.stats(es.topic.shares, "Spanish"),
-#                    category.stats(it.topic.shares, "Italian"),
-#                    category.stats(pt.topic.shares, "Portuguese"))
+#### Topic diversity by category ####
+# Todo: nicer viz (x16) as Deepak suggested.
+topics.by.cat.plot <- ggplot( all.cat.stats, aes(x=category, y=entropy.mean)) + 
+  geom_bar( aes(fill=Language, width=0.7), alpha=1, position="dodge", stat="identity") +  # adjust positon dodge
+  #   geom_text( data=all.cat.stats,
+  #              aes(x=category, 
+  #                  y=entropy.mean, 
+  #                  label=round(entropy.mean,2),
+  #                  size=12,
+  #                  hjust=-0.2, # keep the label afar from the bar
+  #                  vjust=ifelse(Language=="English", 1.2, 0)) ) + # move English label down
+  theme(axis.text.y = element_text(face="bold", colour="#000000", size=16)) + # change axis label font size
+  #theme(legend.position="none") +
+  xlab("") + ylab("Number of topics") +
+  coord_flip()
+print(topics.by.cat.plot)
 
-
-
-# TODO: get edition-wide stats for entropy
-#par(mfrow=c(2,2))
-#boxplot(edition.wide.entropy(en.topic.shares), at=0:2*5+1, 
-#        xlim=c(0,5), ylim=range(1, 5.5))
-#boxplot(edition.wide.entropy(es.topic.shares), at=0:2*5+2, add=T)
-#boxplot(edition.wide.entropy(it.topic.shares), at=0:2*5+3, add=T)
-#boxplot(edition.wide.entropy(pt.topic.shares), at=0:2*5+4, add=T)
-
-# Hack to add missing a row for the missing Royalty category in Spanish
-#all.stats <- rbind(all.stats[1:28,], 
-#                    c("Royalty", as.numeric(0), as.numeric(0),
-#                      as.numeric(0), as.numeric(0), "Spanish"),  
-#                    all.stats[29:63,])
-#rownames(all.stats) <- seq(64)
-
-
-#### Category counts and shares. ###
-# TODO: 
-
-#print(plot.category.shares(subset(all.stats, Language=="English")))
-
-
-# articles.by.cat.plot <- ggplot( all.stats, aes(x=Category, y=percent.articles)) + 
-#   geom_bar( aes(fill=Language, width=0.7), alpha=1, position="dodge", stat="identity") +  # adjust positon dodge
-#   geom_text( data=all.stats,
-#              aes(x=Category, 
-#                  y=percent.articles, 
-#                  label=percent.articles,
-#                  size=12,
-#                  hjust=-0.2, # keep the label afar from the bar
-#                  vjust=ifelse(Language=="English", 1.2, 0)) ) + # move English label down
-#   theme(axis.text.y = element_text(face="bold", colour="#000000", size=16)) + # change axis label font size
-#   #theme(legend.position="right") +
-#   xlab("") + ylab("% of articles") +
-#   coord_flip()
-# print(articles.by.cat.plot)
-
-#### Topics by category ####
-# topics.by.cat.plot <- ggplot( all.stats, aes(x=Category, y=entropy.mean)) + 
-#   geom_bar( aes(fill=Language, width=0.7), alpha=1, position="dodge", stat="identity") +  # adjust positon dodge
-#   geom_text( data=all.stats,
-#              aes(x=Category, 
-#                  y=entropy.mean, 
-#                  label=round(entropy.mean,2),
-#                  size=12,
-#                  hjust=-0.2, # keep the label afar from the bar
-#                  vjust=ifelse(Language=="English", 1.2, 0)) ) + # move English label down
-#   theme(axis.text.y = element_text(face="bold", colour="#000000", size=16)) + # change axis label font size
-#   theme(legend.position="none") +
-#   xlab("") + ylab("Number of topics") +
-#   coord_flip()
-# print(topics.by.cat.plot)
-# 
-# postscript("articles_by_cat_plot.eps")
-# grid.arrange(articles.by.cat.plot, ncol=1, 
-#              widths=unit(c(15), "cm"),
-#              heights=unit(c(13), "cm") )
-# dev.off()
-# 
-# postscript("topics_by_cat_plot.eps")
-# grid.arrange(topics.by.cat.plot, ncol=1, 
-#              widths=unit(c(15), "cm"),
-#              heights=unit(c(13), "cm") )
-# dev.off()
-# 
-# postscript("topics_dist_plot.eps")
-# grid.arrange(topic.dist.plot, ncol=1, 
-#              widths=unit(c(13), "cm"),
-#              heights=unit(c(13), "cm") )
-# dev.off()
